7a1ec42462730d6dfba25539a903b74a3636757e
LeFab70 <kouonang2002@gmail.com>
2025-07-14 19:14:34 -0300

    Initial Commit

---
+++ Lab01_Fabrice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab01_Fabrice
+//
+//  Created by Fabrice Kouonang on 2025-07-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab01_Fabrice/Lab01_FabriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab01_FabriceApp.swift
+//  Lab01_Fabrice
+//
+//  Created by Fabrice Kouonang on 2025-07-14.
+//
+
+import SwiftUI
+
+@main
+struct Lab01_FabriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
