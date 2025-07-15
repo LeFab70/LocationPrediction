c37aa3828813cc94a46d49547acf404a809c885a
LeFab70 <kouonang2002@gmail.com>
2025-07-15 04:34:20 -0300

    all

---
+++ Lab01_Fabrice/ViewsModels/HousePriceMultiModels.swift
@@ -0,0 +1,8 @@
+//
+//  HousePriceMultiModels.swift
+//  Lab01_Fabrice
+//
+//  Created by Fabrice Kouonang on 2025-07-14.
+//
+
+import Foundation
---
+++ Lab01_Fabrice/ViewsModels/HousePriceSimpleModel.swift
@@ -0,0 +1,8 @@
+//
+//  HousePriceSimpleModel.swift
+//  Lab01_Fabrice
+//
+//  Created by Fabrice Kouonang on 2025-07-14.
+//
+
+import Foundation
